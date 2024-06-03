6e4f8c7c18f1532da52025c5e45c0567ac7025ff
XF4rry <christian.pastori.stud@ispascalcomandini.it>
2024-06-03 12:51:20 +0200

    aggiunta menu, modifica gitignore aggiunta

--- views/MainMenuView.ts
+++ views/MainMenuView.ts
@@ -1,7 +1,30 @@
+import * as rl from "readline-sync";
+import { ParcheggiController } from "../controllers/PontosController";
+
 export class MainMenu{
-    static stampaMenu(){
+        static stampaMenu(){
+        let controller = new ParcheggiController();
         console.log("Parcheggio TS");
         console.log("--------------------------------------------------------------------------------------------------------------------");
         console.log("Benvenuto nel Gestionale del Parcheggio");
+        console.log("--------------------------------------------------------------------------------------------------------------------");
+        console.log("1. Visualizza i dati dei veicoli presenti");
+        console.log("2. Visualizza il numero di veicoli presenti");
+        console.log("9. Esci");
+        console.log("--------------------------------------------------------------------------------------------------------------------");
+        let scelta = rl.questionInt("inserire la scelta: ");
+        switch(scelta){
+            case 1:
+                controller.getDatiParcheggiate().forEach(element => {console.log(element.targa + " entrato il: " + element.dateEntry + " e uscito il: " + element.dateExit + " con un importo di: " + element.importo + "€");});
+                break;
+            case 2:
+                console.log(controller.getNumeroVeicoliPresenti());
+                break;
+            case 9:
+                break;
+            default:
+                console.log("scelta non valida");
+                MainMenu.stampaMenu();
+        }
     }
 }
