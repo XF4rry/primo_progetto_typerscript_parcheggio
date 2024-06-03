fe3eba210fafd92fa4aa0667b7582f8dfbf5b229
XF4rry <christian.pastori.stud@ispascalcomandini.it>
2024-06-03 16:28:42 +0200

    da sistemare punto 4 e da fare punto5

--- controllers/PontosController.ts
+++ controllers/PontosController.ts
@@ -1,6 +1,6 @@
 // importo le classi necessarie
 import { Database } from "../models/Database";
-//import { Parcheggio } from "../models/Parcheggio";
+import { Parcheggio } from "../models/Parcheggio";
 import { Veicolo } from "../models/Veicolo";
 
 export class ParcheggiController {
@@ -11,14 +11,13 @@ export class ParcheggiController {
     // inizializzo il database
     this.db = new Database();
   }
-
   //scrivo il metodo getDatiParcheggiate per la prima funzione del problema (stampa i dati dei veicoli presenti)
   public getDatiParcheggiate(): Veicolo[] {
     // Utilizzo il metodo "filter" per prendere i veicoli che non sono ancora usciti
     // Il metodo "filter" filtra l'array in base a una condizione
     // La condizione è che la proprietà "dateExit" di ogni oggetto "Veicolo" sia indefinita
     // L'array filtrato viene restituito
-    return this.db.parcheggio.veicoli.filter((v) => v.dateExit == undefined);
+    return this.db.parcheggio.veicoli.filter((v) => v.dateExit == v.dateEntry);
   }
 
   //scrivo il metodo getNumeroVeicoliPresenti per la seconda funzione del problema (stampa il numero dei veicoli presenti)
@@ -29,8 +28,30 @@ export class ParcheggiController {
     return datiParcheggiate.length;
   }
   addVeicolo(targa: string) : boolean {
+    if(this.getNumeroVeicoliPresenti() < this.db.parcheggio.capienza && this.db.parcheggio.veicoli.find((v) => v.targa == targa) == undefined) {
+    // utilizzo il metodo per evitare ripetizione di codice e controllo la capienza e se la targa non è già presente
+      this.db.parcheggio.veicoli.push(new Veicolo(targa));
     return true;
+    } else {
+      return false;
+    }
   }
 
+  getVeicoloByTarga(targa: string): Veicolo {
+    let veicolo = this.db.parcheggio.veicoli.find((v) => v.targa === targa);
+    if( veicolo == undefined ) {
+      throw new Error('Veicolo non trovato');
+    } else {
+      return veicolo;
+    }
+  }
+
+  calcolaImporto(veicolo: Veicolo): number {
+    veicolo.dateExit = new Date();
+    veicolo.importo = (veicolo.dateExit.getTime() - veicolo.dateEntry.getTime()) * this.db.parcheggio.tariffaOraria;
+    //this.db.parcheggio.veicoli.slice(this.db.parcheggio.veicoli.indexOf(veicolo), 1);
+    return veicolo.importo;
+  }
 }
+
 
--- views/MainMenuView.ts
+++ views/MainMenuView.ts
@@ -11,18 +11,47 @@ export class MainMenuView{
         console.log("--------------------------------------------------------------------------------------------------------------------");
         console.log("1. Visualizza i dati dei veicoli presenti");
         console.log("2. Visualizza il numero di veicoli presenti");
+        console.log("3. Inserisci un veicolo");
+        console.log("4. Rimuovi un veicolo e calcola l'importo");
         console.log("9. Esci");
         console.log("--------------------------------------------------------------------------------------------------------------------");
         let scelta = rl.questionInt("inserire la scelta: ");
         console.clear();
         switch(scelta){
             case 1:
-                controller.getDatiParcheggiate().forEach(element => {console.log(element.targa + " entrato il: " + element.dateEntry + " e uscito il: " + element.dateExit + " con un importo di: " + element.importo + " €");});
+                controller.getDatiParcheggiate().forEach(element => {
+                    if(element.dateExit == element.dateEntry){
+                        console.log(element.targa + " entrato il: " + element.dateEntry);
+                    }else{
+                        console.log(element.targa + " entrato il: " + element.dateEntry + " e uscito il: " + element.dateExit + " importo da pagare: " + element.importo + " €");
+                    };
+                }); 
                 MainMenuView.stampaMenu();
                 break;
             case 2:
                 console.log(controller.getNumeroVeicoliPresenti());
                 MainMenuView.stampaMenu();
+                break;
+            case 3:
+                console.log("Inserisci la targa del veicolo:");
+                let targa = rl.question();
+                if(controller.addVeicolo(targa)){
+                    console.log("veicolo inserito!");
+                    
+                }else{
+                    console.log("parcheggio pieno o veicolo già presente");
+                }
+                MainMenuView.stampaMenu();
+                break;
+            case 4:
+                console.log("Inserisci la targa del veicolo:");
+                let targaExit = rl.question();
+                let veicolo  = controller.getVeicoloByTarga(targaExit);
+                let importo = controller.calcolaImporto(veicolo);
+                console.log(`veicolo ${veicolo.targa} uscito il ${veicolo.dateExit}, importo a pagare: ${veicolo.importo} €`);
+                MainMenuView.stampaMenu();
+                break;
+            case 5:
                 break;
             case 9:
                 
